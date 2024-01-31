7b4b597c74c92aeca24c91c013841a65b65d4cc7
Guilherme Viegas <46000780+Gui-go@users.noreply.github.com>
2024-01-31 21:03:41 +0000

    Update init.sh

--- util-postgres/init/init.sh
+++ util-postgres/init/init.sh
@@ -2,7 +2,8 @@
 
 sleep 10
 
-ogr2ogr -f "PostgreSQL" PG:"dbname=mydb user=user password=passwd host=postgres"  -nlt PROMOTE_TO_MULTI -lco GEOMETRY_NAME=wkb_geometry data/shp_sc/shp_sc.shp
+# ogr2ogr -f "PostgreSQL" PG:"dbname=mydb user=user password=passwd host=postgres"  -nlt PROMOTE_TO_MULTI -lco GEOMETRY_NAME=wkb_geometry data/shp_sc/shp_sc.shp
+ogr2ogr -f "PostgreSQL" PG:"dbname=mydb user=user password=passwd host=10.0.1.4 port=5432" -nlt PROMOTE_TO_MULTI -lco GEOMETRY_NAME=wkb_geometry -nln target_table_name data/shp_sc/shp_sc.shp
 
 
 
