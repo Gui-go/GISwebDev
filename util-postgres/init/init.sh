764af1ed049b8aab22d7cafe101c88de630261ae
Guilherme Viegas <46000780+Gui-go@users.noreply.github.com>
2024-02-01 00:07:12 +0000

    Update init.sh

--- util-postgres/init/init.sh
+++ util-postgres/init/init.sh
@@ -2,13 +2,13 @@
 
 sleep 10
 
-echo "6666666666666666666666666666666666666666666666666666666666666"
+echo "8888888888888888888888888888888888888888888888888888888888888888888888"
 
 ls -a
 
 # ogr2ogr -f "PostgreSQL" PG:"dbname=mydb user=user password=passwd host=postgres"  -nlt PROMOTE_TO_MULTI -lco GEOMETRY_NAME=wkb_geometry data/shp_sc/shp_sc.shp
-ogr2ogr -f "PostgreSQL" PG:"dbname=mydb user=user password=passwd host=postgres port=5432" -nlt PROMOTE_TO_MULTI -lco GEOMETRY_NAME=wkb_geometry -nln target_table_name data/shp_sc/shp_sc.shp
+ogr2ogr -f "PostgreSQL" PG:"dbname=mydb user=user password=passwd host=20.224.150.252 port=5432" -nlt PROMOTE_TO_MULTI -lco GEOMETRY_NAME=wkb_geometry -nln target_table_name data/shp_sc/shp_sc.shp
 
-echo "6666666666666666666666666666666666666666666666666666666666666"
+echo "8888888888888888888888888888888888888888888888888888888888888888888888"
 
 
