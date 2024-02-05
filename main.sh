69629da63b4acb5ae15e5517a20bf60dd9ec7f93
Guilherme Viegas <46000780+Gui-go@users.noreply.github.com>
2024-02-05 10:31:28 +0000

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,24 +1,14 @@
 
 
+sudo apt update -y
+sudo apt upgrade -y
 
+sudo apt install docker
+sudo apt install docker-compose
 
+cd GISwebDev/
 
-# sudo snap install google-cloud-cli --classic
-gcloud auth login
-gcloud config set project pointfln
-
-docker build -t gcr.io/pointfln/pointfln_img:latest .
-
-docker build -t gcr.io/pointfln/pointfln_img:latest -f Dockerfile.cloudrun .
-docker push gcr.io/YOUR_PROJECT_ID/YOUR_IMAGE_NAME:latest
-
-
-docker-compose push
-
-
-
-
-
+sudo docker-compose up --build
 
 
 
