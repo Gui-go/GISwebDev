95daf2bdb6d7152137f3f52ab7a5dc37a4523f03
Guilherme Viegas <46000780+Gui-go@users.noreply.github.com>
2024-02-05 11:12:17 +0000

    Update main.sh

--- main.sh
+++ main.sh
@@ -6,7 +6,7 @@ sudo apt upgrade -y
 sudo apt install docker
 sudo apt install docker-compose
 
-cd GISwebDev/
+cd ~/GISwebDev/
 
 sed -i "s/{{SERVER_IP}}/20.4.22.56/g" nginx/init/nginx.conf
 sed -i "s/{{SERVER_IP}}/20.4.22.56/g" nginx/web/index.html
