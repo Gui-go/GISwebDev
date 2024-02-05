14a170343ed182fee75606bff1f4e0b75476ec6f
Guilherme Viegas <46000780+Gui-go@users.noreply.github.com>
2024-02-05 15:13:36 +0000

    update

--- main.sh
+++ main.sh
@@ -1,29 +1,19 @@
 #!/bin/bash
 
-source ~/GISwebDev/env
-
 cd ~/GISwebDev/
+
+source "./env"
 
 sudo apt update -y
 sudo apt upgrade -y
 
-sudo apt install docker
-sudo apt install docker-compose
+sudo apt install docker -y
+sudo apt install docker-compose -y
 
 
 sed -i "s/{{PUBLIC_IP}}/$SERVER_IP/g" nginx/init/nginx.conf
 sed -i "s/{{PUBLIC_IP}}/$SERVER_IP/g" nginx/web/index.html
 
 sudo docker-compose up --build
-
-
-
-
-
-
-
-
-
-
 
 
--- util-postgres/init/init.sh
+++ util-postgres/init/init.sh
@@ -16,3 +16,8 @@ echo "PostgreSQL is ready now."
 # Run ogr2ogr command
 ogr2ogr -f "PostgreSQL" PG:"dbname=mydb user=user password=passwd host=postgres" -nlt PROMOTE_TO_MULTI -lco GEOMETRY_NAME=wkb_geometry data/shp_sc/shp_sc.shp
 
+if [ $? -eq 0 ]; then
+  echo "ogr2ogr added data/shp_sc/shp_sc.shp into PostgreSQL successfully"
+else
+  echo "ogr2ogr command failed with exit code $?"
+fi
