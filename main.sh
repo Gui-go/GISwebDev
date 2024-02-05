6e5667d3a05689c77cf0f1b0d46bcc76ed2ee94e
Guilherme Viegas <46000780+Gui-go@users.noreply.github.com>
2024-02-05 11:05:44 +0000

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,4 +1,4 @@
-
+#!/bin/bash
 
 sudo apt update -y
 sudo apt upgrade -y
@@ -6,7 +6,10 @@ sudo apt upgrade -y
 sudo apt install docker
 sudo apt install docker-compose
 
-cd GISwebDev/
+# cd GISwebDev/
+
+sed -i "s/{{SERVER_IP}}/20.4.22.56/g" nginx/init/nginx.conf
+sed -i "s/{{SERVER_IP}}/20.4.22.56/g" nginx/web/index.html
 
 sudo docker-compose up --build
 
